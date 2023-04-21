b3c2e2b7c26864b0c1e0fa6b5721544cce33e9b5
raul <jarquinr121@gmail.com>
2023-04-20 18:49:01 -0700

    FIRST LEET twoSum

---
+++ easy/two_sum.py
@@ -0,0 +1,18 @@
+from typing import List
+
+nums = [2,7,11,15]
+target = 9
+
+
+def twoSum(nums: list[int], target : int) -> List[int]:
+    hashmap = dict()
+
+    for i in range(len(nums)):
+        complement = target - nums[i]
+        if nums[i] in hashmap:
+            return [hashmap[nums[i]], i]        #{0,1} returned 
+        else:
+            hashmap[complement] = i     # {7,0} first loop
+
+anw = twoSum(nums, target)
+print(anw)
