106c45c4f9b6fca44e55260e4fbffa9caa29f71d
raulj <112786129+Raulj123@users.noreply.github.com>
2023-12-26 12:45:13 -0800

    binary search/ return index if it was there

--- easy/search_insert_position.py
+++ easy/search_insert_position.py
@@ -1,13 +1,21 @@
 def searchInsert(nums:list[int], target:int) -> int:
-    should_be = 0
-    for i in range(len(nums)):
-        if nums[i] == target:
-            return i 
-        elif nums[i] < target:
-            should_be +=1
-    return should_be
+    l, r = 0, len(nums) -1
+
+    while l <= r:
+        m = ( l + r) // 2
+        if nums[m] > target:
+            r = m - 1
+        elif nums[m] < target:
+            l = m + 1
+        else:
+            return m 
+    return l 
 
 nums = [1,2,6,8,10]
 target = 11
 result = searchInsert(nums, target)
-print("Result: ", result)
+
+if result == 5:
+    print(f"Passed: {result}")
+else:
+    print("Failed")
