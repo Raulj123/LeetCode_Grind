f9afbec750973fd211f773932c46a3c1a509f655
raulj <112786129+Raulj123@users.noreply.github.com>
2024-04-23 13:16:38 -0700

    gave up on day 5 datetime issues

--- MarchMadness/day5.py
+++ MarchMadness/day5.py
@@ -1,11 +1,6 @@
-from datetime import (
-    datetime as DateTime,
-    date as Date,
-    timezone as Timezone,
-    timedelta as TimeDelta,
-)
+from datetime import datetime, timezone, timedelta
 
-with open('5.in') as f:
+with open('5.in',encoding = "utf-8") as f:
     data = f.read().splitlines()
 
 logs = []
@@ -25,9 +20,17 @@ exits = set()  # Set to store exit events
 
 for log in logs_sorted:
     time, name, action, building = log
+    
+
     if action == "->":
         if name in entries:
             print(entries[name])
+            sus_baka = entries[name]
+
+
+            sus = datetime.utcfromtimestamp(sus_baka[0]).strftime('%Y-%m-%dT%H:%SZ')
+            sus = sus.split("T")[0]
+            print(f"SUS time: {sus}")
         else:
             entries[name] = (time, building)
             exits.discard(name)  # Remove from exits if re-entering without exiting
@@ -38,7 +41,25 @@ for log in logs_sorted:
         else:
             print(f"Exit without entry for {name} detected at {time} in building {building}")
 
-# Check for entries without corresponding exits at the end
-for name, (entry_time, building) in entries.items():
-    print(f"No exit for {name} detected at {entry_time} in building {building}")
+sus_p = set()
+e = 0
 
+for log in logs_sorted:
+    time = log[0]
+    name = log[1]
+    action = log[2]
+    
+    time= datetime.utcfromtimestamp(time).strftime('%Y-%m-%dT%H:%SZ')
+    time = time.split("T")[0]
+    # this isnt getting the right time i give up 
+    
+    if time == sus:
+        if action == '->':
+            sus_p.add(name)
+            e += 1
+
+
+
+
+
+print(len(sus_p) * (e))
